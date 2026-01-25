82f00b2da2ed54f294247239d5b50b310e69c23b
Linfeng Li <linfel@Linfengs-MacBook-Air.local>
2026-01-25 09:34:40 -0600

    Created boundary nodes, interior nodes. Created initial triangle mesh. Improvement near the boundary is needed.

---
+++ src/project1/domain.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def build_domain(figname=""):
+
+    file_bladelower = "../../data/project1/bladelower.txt"
+    file_bladeupper = "../../data/project1/bladeupper.txt"
+    bladelower = np.loadtxt(file_bladelower)
+    bladeupper = np.loadtxt(file_bladeupper)
+    bladeupper[:,1] -= 18.0  # Adjust y-coordinates of the upper blade surface
+
+    # x and y coordinates of the blade corners
+    Ax, Ay = bladelower[0,0], bladelower[0,1]
+    By = bladeupper[-1,1]
+    Cy = bladeupper[0,1]
+    Dx, Dy = bladelower[-1,0], bladelower[-1,1]
+
+    # x grid for the periodic boundary group 1 and group 2
+    nBNodes = 69
+    PG1_up_xgrid = np.linspace(Ax-17.0, Ax, nBNodes) # x-nodes on periodic boundaries must match
+    PG1_low_xgrid = PG1_up_xgrid[::-1]                    # reverse
+    PG2_up_xgrid = np.linspace(Dx, Dx+17.0, nBNodes) # x-nodes on periodic boundaries must match
+    PG2_low_xgrid = PG2_up_xgrid[::-1]                    # reverse
+
+    # y grid for the periodic boundary group 1 and group 2
+    PG1_up_ygrid  = np.full(nBNodes, Ay)
+    PG1_low_ygrid = np.full(nBNodes, By)
+    PG2_up_ygrid  = np.full(nBNodes, Dy)
+    PG2_low_ygrid = np.full(nBNodes, Cy)
+
+    # inflow and outflow boundaries
+    nBNodes_inout = 73
+    inflow_xgrid = np.full(nBNodes_inout, Ax-17.0)
+    inflow_ygrid = np.linspace(By, Ay, nBNodes_inout)
+    outflow_xgrid = np.full(nBNodes_inout, Dx+17.0)
+    outflow_ygrid = np.linspace(Dy, Cy, nBNodes_inout)
+
+    # x-nodes on boundary 1 and 2 match with each other
+    boundary_1 = np.column_stack((PG1_up_xgrid, PG1_up_ygrid))[:-1, :]
+    boundary_2 = bladelower[:-1, :]
+    boundary_3 = np.column_stack((PG2_up_xgrid, PG2_up_ygrid))[:-1, :]
+    boundary_4 = np.column_stack((outflow_xgrid, outflow_ygrid))[:-1, :]
+    boundary_5 = np.column_stack((PG2_low_xgrid, PG2_low_ygrid))[:-1, :]
+    boundary_6 = bladeupper[:-1, :]
+    boundary_7 = np.column_stack((PG1_low_xgrid, PG1_low_ygrid))[:-1, :]
+    boundary_8 = np.column_stack((inflow_xgrid, inflow_ygrid))[:-1, :]
+
+    # combine all boundaries into a single array if needed
+    boundaries = np.vstack((boundary_1, boundary_2, boundary_3, boundary_4,
+                            boundary_5, boundary_6, boundary_7, boundary_8))
+
+    # find the extents of the domain
+    x_min, x_max = np.min(boundaries[:, 0]), np.max(boundaries[:, 0])
+    y_min, y_max = np.min(boundaries[:, 1]), np.max(boundaries[:, 1])
+    extent = [x_min, x_max, y_min, y_max]
+
+    # Plotting the domain boundaries if a figure name is provided
+    if (not not figname): 
+        # Create the plot
+        plt.figure(figsize=(12, 8))
+
+        # Periodic Boundaries
+        plt.plot(boundary_1[:, 0], boundary_1[:, 1], '.', ms=3, color='tab:green',  label='B1: PG1 Top')
+        plt.plot(boundary_3[:, 0], boundary_3[:, 1], '.', ms=3, color='tab:purple', label='B3: PG2 Top')
+        plt.plot(boundary_5[:, 0], boundary_5[:, 1], '.', ms=3, color='tab:pink',   label='B5: PG2 Bottom')
+        plt.plot(boundary_7[:, 0], boundary_7[:, 1], '.', ms=3, color='tab:olive',  label='B7: PG1 Bottom')
+
+        # Inflow and Outflow
+        plt.plot(boundary_8[:, 0], boundary_8[:, 1], '.', ms=3, color='black',      label='B8: Inflow')
+        plt.plot(boundary_4[:, 0], boundary_4[:, 1], '.', ms=3, color='tab:orange', label='B4: Outflow')
+
+        # Blade Surfaces
+        plt.plot(boundary_2[:, 0], boundary_2[:, 1], '.', ms=3, color='tab:red',    label='B2: Blade Lower')
+        plt.plot(boundary_6[:, 0], boundary_6[:, 1], '.', ms=3, color='tab:blue',   label='B6: Blade Upper')
+
+        # Formatting the plot for your report
+        plt.xlabel('x (mm)')
+        plt.ylabel('y (mm)')
+        plt.title('Discretized Domain Boundary Segments')
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+        plt.axis('equal') 
+        plt.grid(True, linestyle=':', alpha=0.4)
+        plt.tight_layout()
+
+        plt.savefig(figname, dpi=200, bbox_inches='tight', pad_inches=0.1)
+    
+    return boundaries, extent
+
+if __name__ == "__main__":
+    build_domain("../../output/project1/domain_boundary.png")
+    #build_domain()
---
+++ src/project1/mesh.py
@@ -0,0 +1,94 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from scipy.spatial import Delaunay
+from domain import build_domain
+
+def equilateral_tri_mesh(extent,a):
+    """
+    extent: list of 4 floats [x_min, x_max, y_min, y_max]
+    a: edge length of an equilateral triangle
+    """
+    a = float(a)
+
+    # Calculate height of the equilateral triangle
+    h = a * np.sqrt(3.) / 2.
+    
+    # Calculate number of nodes
+    nx = int(np.ceil((extent[1] - extent[0]) / a)) + 2  # number of nodes in x direction
+    ny = int(np.ceil((extent[3] - extent[2]) / h)) + 2  # number of nodes in y direction
+
+    # Generate basic grid
+    x = np.arange(nx) * a
+    y = np.arange(ny) * h
+    X, Y = np.meshgrid(x, y)
+
+    # Apply global offsets
+    X += extent[0]
+    Y += extent[2]
+
+    # avoid boundary touching by shifting grid slightly
+    X -= a/4.; Y -= h/2.
+
+    # Apply offsets to every second row to create equilateral triangles
+    X[1::2, :] += a/2.
+
+    return X, Y
+
+def generate_unstructured_mesh(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Build initial equilateral triangle mesh
+    boundary, extent = build_domain()
+    a = 1.5     # edge length of the equilateral triangle
+    X, Y = equilateral_tri_mesh(extent, a)
+
+    # Check which nodes are inside the boundary
+    outer_path = Path(boundary)
+    grid_points = np.vstack((X.flatten(), Y.flatten())).T
+    inside_mask = outer_path.contains_points(grid_points)
+    inside_nodes = grid_points[inside_mask]
+
+    # Plot the nodes and boundary
+    plt.figure(figsize=(12, 8))
+    plt.plot(inside_nodes[:,0], inside_nodes[:,1], 'ko', ms=1)
+    plt.plot(boundary[:, 0], boundary[:, 1], 'o', ms=1, color='tab:orange',
+            label='Boundary')
+    plt.axis('equal')
+    plt.savefig(os.path.join(output_dir, 'nodes.png'), dpi=300, bbox_inches='tight', pad_inches=0.1)
+    plt.close()
+
+    # Combine boundary and inside nodes
+    all_nodes = np.vstack((boundary, inside_nodes))
+
+    # Perform Delaunay triangulation
+    tri = Delaunay(all_nodes)
+    simp = tri.simplices # triangle simplices defined by indices of all_nodes
+    
+    # remove triangles whose centroids are outside the boundary
+    centroids = np.mean(all_nodes[simp], axis=1)
+    inside_centroid_mask = outer_path.contains_points(centroids)
+    simp = simp[inside_centroid_mask]
+
+    # Save nodes coordinates
+    np.savetxt(os.path.join(output_dir, "boundary_nodes.txt"), boundary, fmt='%.4f', delimiter=',')
+    np.savetxt(os.path.join(output_dir, "inside_nodes.txt"), inside_nodes, fmt='%.4f', delimiter=',')
+    np.savetxt(os.path.join(output_dir, "all_nodes.txt"), all_nodes, fmt='%.4f', delimiter=',')
+
+    # Save elements with 1-based indexing
+    np.savetxt(os.path.join(output_dir, "E.txt"), simp+1, fmt='%d', delimiter=',')
+
+    # Plot the triangulation
+    plt.figure(figsize=(12, 8))
+    plt.triplot(all_nodes[:, 0], all_nodes[:, 1], simp, color='gray', lw=0.5)
+    #plt.triplot(all_nodes[:, 0], all_nodes[:, 1], tri.simplices, color='gray', lw=0.5)
+    plt.savefig(os.path.join(output_dir, 'tri_mesh.png'), dpi=300, bbox_inches='tight', pad_inches=0.1)
+    plt.show()
+    plt.close()
+
+    # return all nodes, boundary nodes, inside nodes, and triangle elements
+    return all_nodes, boundary, inside_nodes, simp
+
+if __name__ == "__main__":
+    generate_unstructured_mesh(output_dir='../../output/project1/')
